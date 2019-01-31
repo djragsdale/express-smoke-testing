9cfdb0fb36a549850bbdcbff293a3030f907a5c4
David Ragsdale <me@davidjragsdale.com>
2019-01-30 21:33:44 -0500

    Implement basic express static site and API

---
+++ app.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+'use strict';
+
+const bodyParser = require('body-parser');
+const debug = require('debug')('express-smoke-testing');
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+
+const PORT = process.env.PORT || '3000';
+
+const app = express();
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const data = {
+  companies: [
+    {
+      name: 'Google',
+    },
+    {
+      name: 'Apple',
+    },
+    {
+      name: 'Microsoft',
+    },
+  ],
+};
+
+const router = express.Router();
+
+router
+  .get('/companies', (req, res) => {
+    res.status(200).send(data.companies);
+  })
+  .get('/company/:id', (req, res) => {
+    const { id } = req.params;
+    if (data.companies[id]) {
+      res.status(200).send(data.companies[id]);
+    } else {
+      res.status(400).end();
+    }
+  });
+
+app.use('/api', router);
+app.use(express.static(path.join(__dirname, './public')));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
