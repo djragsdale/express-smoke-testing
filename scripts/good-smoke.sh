44075193ab4b58e1d08bdb8ceb6a9339c499a9ae
David Ragsdale <me@davidjragsdale.com>
2019-01-31 10:04:37 -0500

    Remove sleep command from smoke test script

--- scripts/good-smoke.sh
+++ scripts/good-smoke.sh
@@ -2,6 +2,4 @@
 dredd --config dredd-api.yml
 ps aux | grep "node serve.js"
 ps aux | grep "node serve.js" | grep -v grep | awk '{print $2}' | xargs kill -9
-echo "Waiting 5 seconds for port to close"
-sleep 5s
 dredd --config dredd-pages.yml
