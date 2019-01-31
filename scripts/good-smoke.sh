3b0a75eb6944e910bba329fd3842f389db9b1c01
David Ragsdale <me@davidjragsdale.com>
2019-01-31 10:24:37 -0500

    Attempt Dredd debugging

--- scripts/good-smoke.sh
+++ scripts/good-smoke.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 dredd --config dredd-api.yml
-ps aux | grep "node serve.js"
-ps aux | grep "node serve.js" | grep -v grep | awk '{print $2}' | xargs kill -9
-dredd --config dredd-pages.yml
+dredd --config dredd-pages.yml --level=debug
