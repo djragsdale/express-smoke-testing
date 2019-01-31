1d1de3fcf4abf10f58119cf1b3cb3a2fb4a077b8
David Ragsdale <me@davidjragsdale.com>
2019-01-31 09:59:25 -0500

    Attempt to manually kill server process

--- scripts/good-smoke.sh
+++ scripts/good-smoke.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 dredd --config dredd-api.yml
-ps aux
+ps aux | grep "node serve.js"
+ps aux | grep "node serve.js" | grep -v grep | awk '{print $2}' | xargs kill -9
 echo "Waiting 5 seconds for port to close"
 sleep 5s
 dredd --config dredd-pages.yml
